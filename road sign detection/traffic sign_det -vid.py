81cd119fc3b7e6924b99eb99942b6000cd40ad97
Venu Pabbuleti <117000362+VenuPabbuleti@users.noreply.github.com>
2023-07-11 16:20:16 +0530

    Add files via upload

---
+++ road sign detection/traffic sign_det -vid.py
@@ -0,0 +1,55 @@
+import cv2 as cv
+import numpy as np
+
+# Object detector constant 
+CONFIDENCE_THRESHOLD = 0.4
+NMS_THRESHOLD = 0.3
+
+# colors for object detected
+COLORS = [(255,0,0),(255,0,255),(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+GREEN =(0,255,0)
+BLACK =(0,0,0)
+# defining fonts 
+FONTS = cv.FONT_HERSHEY_COMPLEX
+
+# getting class names from classes.txt file 
+class_names = []
+with open("classes.txt", "r") as f:
+    class_names = [cname.strip() for cname in f.readlines()]
+    
+#  setttng up opencv net
+yoloNet = cv.dnn.readNet('yolov4-tiny-custom_last.weights', 'yolov4-tiny-custom.cfg')
+
+yoloNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
+yoloNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA_FP16)
+
+model = cv.dnn_DetectionModel(yoloNet)
+model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
+ 
+
+# object detector funciton /method
+def od(image):
+    classes, scores, boxes = model.detect(image, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+    # creating empty list to add objects data
+    data_list=[]
+    for (classid, score, box) in zip(classes, scores, boxes):
+        # define color of each, object based on its class id 
+        color= COLORS[int(classid) % len(COLORS)]
+        label = "%s : %f" % (class_names[classid[0]], score)
+        
+        cv.rectangle(image, box, color, 2)
+        cv.putText(image, label, (box[0], box[1]-14), FONTS, 0.5, color, 2)
+        print(label)
+       
+
+ 
+#
+cap=cv.VideoCapture(0,cv.CAP_DSHOW)
+while True:
+    ret,frame=cap.read()
+    od(frame)
+    cv.imshow('frame',frame)
+    if cv.waitKey(1) ==ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindow()
